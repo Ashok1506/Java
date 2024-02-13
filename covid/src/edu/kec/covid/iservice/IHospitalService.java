f65bc734e7b7cd4d0c8ea05d6729eaeeedfd208f
Ashok1506 <105486007+Ashok1506@users.noreply.github.com>
2024-02-13 11:06:30 +0530

    Delete covid directory

--- covid/src/edu/kec/covid/iservice/IHospitalService.java
+++
@@ -1,5 +0,0 @@
-package edu.kec.covid.iservice;
-
-public interface IHospitalService {
-	public Integer findNearByHospital(Double latitude,Double longitude);
-}
--- covid/src/edu/kec/covid/iservice/IPatientService.java
+++
@@ -1,5 +0,0 @@
-package edu.kec.covid.iservice;
-import edu.kec.covid.model.*;
-public interface IPatientService {
-	public void insert(Patient patient);
-}
